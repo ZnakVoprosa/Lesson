7569fda13853b4dd1600f7d0e8ca5b0d87f6bf26
ZnakVoprosa <ruslanpidakov@gmail.com>
2026-03-11 03:51:50 +0300

    Update day_screen.dart

--- lib/screens/day_screen.dart
+++ lib/screens/day_screen.dart
@@ -1,1 +1,20 @@
-iii
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/schedule_provider.dart';
+
+class DayScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final prov = Provider.of<ScheduleProvider>(context);
+    return PageView.builder(
+      itemCount: 7,
+      itemBuilder: (ctx, i) {
+        final list = prov.lessons.where((l) => l.dayOfWeek == i + 1 && (l.weekType == 0 || l.weekType == prov.currentViewWeek)).toList();
+        return ListView.builder(
+          itemCount: list.length,
+          itemBuilder: (ctx, idx) => ListTile(title: Text(list[idx].title), subtitle: Text(list[idx].time)),
+        );
+      },
+    );
+  }
+}
