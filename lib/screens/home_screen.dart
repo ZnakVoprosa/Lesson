2dd5fa4060cb02c6847ca4a59d10d92339d9c662
ZnakVoprosa <ruslanpidakov@gmail.com>
2026-03-11 03:52:24 +0300

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,1 +1,22 @@
-iii
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/schedule_provider.dart';
+import 'day_screen.dart';
+import 'week_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(title: Text("Расписание"), bottom: TabBar(tabs: [Tab(text: "День"), Tab(text: "Неделя")])),
+        body: TabBarView(children: [DayScreen(), WeekScreen()]),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () => Navigator.pushNamed(context, '/add'),
+        ),
+      ),
+    );
+  }
+}
