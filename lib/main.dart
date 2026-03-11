4966e8db74ed41c5367c2f197b8e859e71eaf40a
ZnakVoprosa <ruslanpidakov@gmail.com>
2026-03-11 03:41:20 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,20 @@
-void main() {
-  // Placeholder for the main entry point
-  print('Hello, World!');
-}
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'providers/schedule_provider.dart';
+import 'screens/home_screen.dart';
+import 'screens/add_lesson_screen.dart'; // <--- Импортируем экран формы
+
+void main() => runApp(
+  ChangeNotifierProvider(
+    create: (_) => ScheduleProvider()..loadFromFile('default'),
+    child: MaterialApp(
+      debugShowCheckedModeBanner: false, // Убираем надпись "Debug"
+      theme: ThemeData.dark(),
+      home: HomeScreen(),
+      // Вот здесь мы прописываем маршруты:
+      routes: {
+        '/add': (context) => AddLessonScreen(),
+      },
+    ),
+  ),
+);
