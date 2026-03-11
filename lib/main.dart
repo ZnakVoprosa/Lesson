ba01173d49e6f9786275f2d69ae277f330326731
ZnakVoprosa <ruslanpidakov@gmail.com>
2026-03-11 16:20:01 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'providers/schedule_provider.dart';
 import 'screens/home_screen.dart';
-import 'screens/add_lesson_screen.dart'; // <--- Импортируем экран формы
 
-void main() => runApp(
-  ChangeNotifierProvider(
-    create: (_) => ScheduleProvider()..loadFromFile('default'),
-    child: MaterialApp(
-      debugShowCheckedModeBanner: false, // Убираем надпись "Debug"
-      theme: ThemeData.dark(),
-      home: HomeScreen(),
-      // Вот здесь мы прописываем маршруты:
-      routes: {
-        '/add': (context) => AddLessonScreen(),
-      },
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ScheduleProvider())
+      ],
+      child: MaterialApp(
+        title: 'My Schedule',
+        theme: ThemeData.dark(),
+        home: HomeScreen(),
+        onGenerateRoute: (_) => MaterialPageRoute(builder: (_) => HomeScreen()),
+      ),
     ),
-  ),
-);
+  );
+}
