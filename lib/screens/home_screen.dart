2858451a541f6e81343d0353a4bd19764f3bd4b9
ZnakVoprosa <ruslanpidakov@gmail.com>
2026-03-11 16:22:23 +0300

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,15 +7,12 @@ import 'week_screen.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(title: Text("Расписание"), bottom: TabBar(tabs: [Tab(text: "День"), Tab(text: "Неделя")])),
-        body: TabBarView(children: [DayScreen(), WeekScreen()]),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () => Navigator.pushNamed(context, '/add'),
-        ),
+    return Scaffold(
+      appBar: AppBar(title: Text('My Schedule')),
+      body: Column(
+        children: [
+          Expanded(child: DayScreen()),
+        ],
       ),
     );
   }
