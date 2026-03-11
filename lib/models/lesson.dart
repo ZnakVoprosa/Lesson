9f799569ba24ea40f17f609ec587668f7d682a4d
ZnakVoprosa <ruslanpidakov@gmail.com>
2026-03-11 16:20:39 +0300

    Update lesson.dart

--- lib/models/lesson.dart
+++ lib/models/lesson.dart
@@ -1,12 +1,43 @@
+import 'dart:convert';
+
 class Lesson {
-  final String title, type, room, time, teacher, items;
-  final int dayOfWeek, weekType;
+  final String id, title, type, room, time, teacher, items;
+  final int dayOfWeek;
+  final int weekType;
 
-  Lesson({required this.title, required this.type, required this.room, required this.time, 
-          required this.teacher, required this.items, required this.dayOfWeek, required this.weekType});
+  Lesson({
+    required this.id,
+    required this.title,
+    required this.type,
+    required this.room,
+    required this.time,
+    required this.teacher,
+    required this.items,
+    required this.dayOfWeek,
+    required this.weekType,
+  });
 
-  Map<String, dynamic> toJson() => {'t': title, 'ty': type, 'r': room, 'ti': time, 'te': teacher, 'i': items, 'd': dayOfWeek, 'w': weekType};
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'title': title,
+    'type': type,
+    'room': room,
+    'time': time,
+    'teacher': teacher,
+    'items': items,
+    'dayOfWeek': dayOfWeek,
+    'weekType': weekType,
+  };
+
   factory Lesson.fromJson(Map<String, dynamic> json) => Lesson(
-    title: json['t'], type: json['ty'], room: json['r'], time: json['ti'], 
-    teacher: json['te'], items: json['i'], dayOfWeek: json['d'], weekType: json['w']);
+    id: json['id'],
+    title: json['title'],
+    type: json['type'],
+    room: json['room'],
+    time: json['time'],
+    teacher: json['teacher'],
+    items: json['items'],
+    dayOfWeek: json['dayOfWeek'],
+    weekType: json['weekType'],
+  );
 }
